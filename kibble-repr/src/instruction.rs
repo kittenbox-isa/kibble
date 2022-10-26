0ca9e17af499ffc59ba14994b27c3bab16c85cef
Avery R <contact@nekodjin.org>
2022-10-25 22:14:17 -0400

    fhdsklfhdsjlk

--- kibble-repr/src/instruction.rs
+++ kibble-repr/src/instruction.rs
@@ -15,36 +15,3 @@ pub enum Instruction {
         offs: Immediate<15>,
     },
 }
-
-impl Instruction {
-    pub fn encoding(self) -> u32 {
-        use Instruction::*;
-
-        let mut bits = 0;
-
-        match self {
-            LDLI { dest, imm } => {
-                bits |= *self.opcode();
-            }
-            LOAD { dest, addr, offs } => {
-                bits |= *self.opcode();
-                bits |= *dest.encoding() << 20;
-                bits |= *addr.encoding() << 15;
-                bits |= *offs;
-            }
-        }
-
-        bits
-    }
-
-    fn opcode(self) -> Immediate<7> {
-        use Instruction::*;
-
-        let bits = match self {
-            LDLI { .. } => 0b0000000,
-            LOAD { .. } => 0b0000010,
-        };
-
-        Immediate::new(bits)
-    }
-}
